aeb98f0f30fb74f67f6bb111f2e10c473d8ba6f4
Daniel Warfield <danielwarfield1@gmail.com>
2023-03-30 17:05:42 +0000

    Update 3 files

--- src/Components/ResumeView.js
+++ src/Components/ResumeView.js
@@ -1,7 +1,18 @@
+import resumeImg from "./resources/resume/resumeImg.png";
+
 export default function ResumeView() {
   return (
     <div>
-      <h1>Foo</h1>
+      <h1>Resume</h1>
+      <div style={{ margin: "10px" }}>
+        <img style={{ maxWidth: "200px" }} src={resumeImg} />
+      </div>
+      <a
+        style={{ color: "black" }}
+        href="https://drive.google.com/file/d/12xz01qeA1HBZY_RHcvS-oftGT4yOTsKy/view?usp=share_link"
+      >
+        View .pdf
+      </a>
     </div>
   );
 }
