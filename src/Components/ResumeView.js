d8fc346e52a7da03308a27b19a013669c6b8422a
Daniel Warfield <danielwarfield1@gmail.com>
2023-04-27 22:15:50 +0000

    added cover letter

--- src/Components/ResumeView.js
+++ src/Components/ResumeView.js
@@ -1,4 +1,5 @@
 import resumeImg from "./resources/resume/resumeImg.png";
+import coverImg from "./resources/resume/coverletterimg.jpg";
 
 export default function ResumeView() {
   return (
@@ -10,6 +11,16 @@ export default function ResumeView() {
       <a
         style={{ color: "black" }}
         href="https://drive.google.com/file/d/12xz01qeA1HBZY_RHcvS-oftGT4yOTsKy/view?usp=share_link"
+      >
+        View .pdf
+      </a>
+      <h1>Cover Letter</h1>
+      <div style={{ margin: "10px" }}>
+        <img style={{ maxWidth: "200px" }} src={coverImg} />
+      </div>
+      <a
+        style={{ color: "black" }}
+        href="https://drive.google.com/file/d/1POpY8niId0ZeCzqwLXXHO0GRIE7cj0oy/view?usp=sharing"
       >
         View .pdf
       </a>
